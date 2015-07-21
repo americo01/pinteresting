8eb9dac34f35837ffe5bb0ca49581675d460b9cb
americo@bitlab.mx <americo@bitlab.mx>
2015-07-21 15:25:47 -0500

    hard fix try

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717233322) do
+ActiveRecord::Schema.define(version: 20150721183853) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "image"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
@@ -35,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150717233322) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
